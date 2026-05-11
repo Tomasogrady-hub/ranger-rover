ad2729e97d31e956e3cb423eb3f40ae44501b4f8
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-05-11 10:14:25 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v6';
+const CACHE_NAME = 'ranger-rover-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
