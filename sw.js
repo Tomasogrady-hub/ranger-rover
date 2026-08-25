9668a77a1986f2d3e1c7ebaefb381415893afe10
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-08-25 16:38:36 -0700

    Bump cache to ranger-rover-v10-54

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v10-53';
+const CACHE_NAME = 'ranger-rover-v10-54';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
