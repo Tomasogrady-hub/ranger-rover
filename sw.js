6244222fca13ba2985926f3bb046421315a4d14a
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-08-26 16:47:04 -0700

    v10.34/v10.59: composed record title (Ranger + template + years) for list and PDF filename, no timestamp suffix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v10-58';
+const CACHE_NAME = 'ranger-rover-v10-59';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
