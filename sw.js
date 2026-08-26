d3fcc26120f47f3cfbc79a05cd4d91e7b9deba2f
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-08-26 15:52:53 -0700

    v10.57: date pickers for Date/Start Date/End Date, remove Unit field

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v10-56';
+const CACHE_NAME = 'ranger-rover-v10-57';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
