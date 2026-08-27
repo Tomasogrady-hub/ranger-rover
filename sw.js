988790a14ed09686aa370ec97f1f89da8b8350c2
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-08-26 17:07:26 -0700

    v10.35/v10.60: Send Record to dropdown + rule-based email sending, Data>People bulk Send a Form Record action

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v10-59';
+const CACHE_NAME = 'ranger-rover-v10-60';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
