0207f382ba4a06d576aac34f28a14f8efe9e3cac
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-08-29 20:48:48 -0700

    Schools tab: replace filter chips with 6 dropdown filters (Program/Placement/Ranger/Day/PO/Units) v10.71

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v10-70';
+const CACHE_NAME = 'ranger-rover-v10-71';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
