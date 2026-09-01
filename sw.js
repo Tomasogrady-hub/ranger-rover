9721a40f8225b00153118e8dc273f78fb07e4ec4
Tomasogrady-hub <Tomasogrady@enrichla.org>
2026-09-01 13:11:13 -0700

    v10.78: None option for By Ranger and By Day of Week filters

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ranger-rover-v10-77';
+const CACHE_NAME = 'ranger-rover-v10-78';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
